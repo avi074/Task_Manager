1409df51737f31b9e3684466f5c58a4ccf27d490
Avi074 <gautamavinash.ag@gmail.com>
2024-11-15 17:36:55 +0530

    addBtn added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,25 @@
-function Header() {
+import { Button } from "@material-tailwind/react";
+import todoIcon from "../assets/todo.svg";
+import addIcon from "../assets/add.svg";
+
+function Header({ events: { addTask } }) {
   return (
     <>
-      <header className="p-4 shadow-lg rounded-b-xl border-2 ibm-plex-mono-semibold border-gray-300 bg-gray-50 flex">
-        <h1 className="ml-4 uppercase text-3xl text-blue-800">Task Manager</h1>
-        <input type="search" placeholder="Search tasks..." className="px-2 ml-auto mr-4 outline-none ring-2 ring-gray-600 block w-1/2 rounded-md"/>
+      <header className='p-4 shadow-lg rounded-b-xl border-2 ibm-plex-mono-semibold border-gray-300 bg-gray-50 flex items-center'>
+        <img src={todoIcon} alt='' className='size-8' />
+        <h1 className='ml-4 uppercase text-3xl text-blue-800'>Task Manager</h1>
+        <Button
+          variant='gradient'
+          color='green'
+          className='p-1 flex gap-2 items-center mx-auto md:mr-8 md:ml-auto rounded-xl'
+          onClick={addTask}>
+          <img
+            src={addIcon}
+            alt=''
+            className='size-8 rounded-full border-2 border-green-600 bg-white'
+          />
+          <span className='text-lg mr-2 hidden md:block'>Add Task</span>
+        </Button>
       </header>
     </>
   );
