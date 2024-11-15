327c681be4c5883eef84b55ce0b6abfedf64466d
Avi074 <gautamavinash.ag@gmail.com>
2024-11-15 17:36:10 +0530

    constants added

---
+++ src/utils/constants.js
@@ -0,0 +1,7 @@
+const PRIORITY = Object.freeze(['Low', 'Medium', 'High', 'Critical', 'Urgent'])
+const PCOLOR = Object.freeze(['blue', 'green', 'yellow', 'orange', 'red'])
+const STATUS = Object.freeze(['Completed', 'Pending', 'Active'])
+const SCOLOR = Object.freeze(['gray', 'amber', 'green'])
+const VAR_NAME = 'local-todos-store';
+
+export { PRIORITY, PCOLOR, STATUS, SCOLOR, VAR_NAME }
--- src/utils/taskStore.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import taskReducer from "./taskSlice";
-const taskStore = configureStore({
-  reducer: {
-    tasks: taskReducer,
-  },
-});
-
-export default taskStore;
